52575e10aff8cc47b19dfc5b1ca8667886064254
ghacupha <mailnjeru@gmail.com>
2018-08-05 12:28:04 +0300

    Removed phase declaration in ms-fonts since the profile should be phase agnostic

--- src/scripts/install-ms-fonts.sh
+++ src/scripts/install-ms-fonts.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# accept EULA
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 
-# install font
 sudo apt-get install ttf-mscorefonts-installer
